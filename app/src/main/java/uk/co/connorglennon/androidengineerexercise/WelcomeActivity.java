33a074d0df774a7b48fb0002ea6380d976e67f6f
Fat-Llama <connorglennon@outlook.com>
2017-11-16 22:29:52 +0000

    Fucked it mate

--- app/src/main/java/uk/co/connorglennon/androidengineerexercise/WelcomeActivity.java
+++ app/src/main/java/uk/co/connorglennon/androidengineerexercise/WelcomeActivity.java
@@ -8,6 +8,6 @@ public class WelcomeActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sign_up);
+        setContentView(R.layout.activity_account_details);
     }
 }
