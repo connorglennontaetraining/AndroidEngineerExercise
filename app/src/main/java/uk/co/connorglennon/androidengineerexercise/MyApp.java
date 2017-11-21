bb1311a3be3bdf299e988cb6d92072314b24fc3b
Fat-Llama <connorglennon@outlook.com>
2017-11-21 14:32:16 +0000

    Javadoc and clean up on MyApp

--- app/src/main/java/uk/co/connorglennon/androidengineerexercise/MyApp.java
+++ app/src/main/java/uk/co/connorglennon/androidengineerexercise/MyApp.java
@@ -4,48 +4,121 @@ import android.app.Application;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.graphics.drawable.Drawable;
-import android.security.keystore.KeyGenParameterSpec;
-import android.security.keystore.KeyProperties;
 import android.util.Base64;
 
-import java.security.KeyPair;
-import java.security.KeyPairGenerator;
-import java.security.NoSuchAlgorithmException;
-import java.security.NoSuchProviderException;
 import java.security.SecureRandom;
 
 import io.realm.Realm;
 import io.realm.RealmConfiguration;
-import uk.co.connorglennon.androidengineerexercise.realm.RealmController;
-
-/**
- * Created by Connor Glennon on 18/11/2017.
- */
 
 public class MyApp extends Application {
-    public static Drawable tick, cross;
+
+    ///////////////////////////////////////////////////////////////////////////////
+    // Static variables.
+    ///////////////////////////////////////////////////////////////////////////////
+    /* Application context. */
     public static Context appContext;
 
-    public static byte[] key;
+    /* Shared preferences. */
     public static SharedPreferences sharedPreferences;
     public static SharedPreferences.Editor sharedPreferencesEditor;
+
+    /* Global drawables. */
+    public static Drawable tick, cross;
+
+
+
+    ///////////////////////////////////////////////////////////////////////////////
+    // Member variables.
+    ///////////////////////////////////////////////////////////////////////////////
+
 
     @Override
     public void onCreate() {
         super.onCreate();
 
+        // avoid moving this initialisation.
+        appContext = this;
+        init();
+    }
+
+
+
+    ///////////////////////////////////////////////
+    /* Init section */
+    ///////////////////////////////////////////////
+
+    /**
+     * initialises all the necessary pieces of the app
+     * that are required when the app starts.
+     * Add any init functionality here by creating an call
+     * to your init method and adding it to the list.
+     * be careful of the order of inits.
+     */
+    public void init()
+    {
+        // Always call this method as high up as possible.
+        initSharedPreferences();
+        initGlobalDrawables();
+        initAsycn();
+    }
+
+    /**
+     * Asynchronous method to speed up application start time.
+     * Add code that can run in the background with no errors
+     * and doesn't need to completed by the time onCreate is finished.
+     */
+    private void initAsycn()
+    {
+        Thread initThread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                // initialise Realm database.
+                initRealm();
+
+                //initialise GreenDAO
+                //initGreenDao();
+            }
+        });
+        initThread.start();
+    }
+
+    /**
+     * Some drawables are used programmatically in various parts of the application.
+     * Defined here they are less likely to be created more than once.
+     * They can be used in methods with a context to call 'getDrawable(id)'
+     */
+    private void initGlobalDrawables()
+    {
         tick = getDrawable(R.drawable.tick);
         cross = getDrawable(R.drawable.cross);
+    }
 
+    /**
+     * initialises the shared preferences objects of this class.
+     * Use this method to initialise any shared preferences that
+     * need setting at application creation time. Don't bloat the
+     * onCreate() method with preference creation.
+     */
+    private void initSharedPreferences()
+    {
         sharedPreferences = getSharedPreferences("uk.co.connorglennon.androidengineerexercise", MODE_PRIVATE);
         sharedPreferencesEditor = sharedPreferences.edit();
+    }
 
+    /**
+     * initialises realm with encryption enabled.
+     */
+    private void initRealm()
+    {
+        //TODO: look at moving this key to a secure location.
         if(sharedPreferences.getString("ENCKEY", null) == null)
         {
-            sharedPreferencesEditor.putString("ENCKEY", genEncKey()).apply();
+            byte[] key = new byte[64];
+            new SecureRandom().nextBytes(key);
+            String enckey = Base64.encodeToString(key, Base64.DEFAULT);
+            sharedPreferencesEditor.putString("ENCKEY", enckey).apply();
         }
-
-        appContext = this;
 
         Realm.init(this);
 
@@ -58,17 +131,4 @@ public class MyApp extends Application {
 
         Realm.setDefaultConfiguration(configuration);
     }
-
-    private String genEncKey() {
-        key = new byte[64];
-        new SecureRandom().nextBytes(key);
-        return Base64.encodeToString(key, Base64.DEFAULT);
-    }
-
-    @Override
-    public void onTerminate() {
-        super.onTerminate();
-        RealmController.getInstance().close();
-    }
-
 }
